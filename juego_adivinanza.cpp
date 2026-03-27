d7cb62bb928d9f6450f5d58f35e31a1ad437b041
RenacuajoSal <rene.salazar@ucb.edu.bo>
2026-03-27 13:00:47 -0400

    2026-03-20_01 programa de adivinanza en c++

---
+++ juego_adivinanza.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+cout<<"*************************"<<endl;
+cout<<"*Bienvenido al juego de la Adivinanza!*"<<endl;
+cout<<"*************************"<<endl;
+return 0;
+}
